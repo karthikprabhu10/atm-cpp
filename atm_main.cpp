be013d69436be3c301cd30070fcc2f168210ea4d
K4 <108575187+karthikprabhu10@users.noreply.github.com>
2023-10-14 12:30:37 +0530

    Update atm_main.cpp

--- atm_main.cpp
+++ atm_main.cpp
@@ -3,21 +3,23 @@
 #include<conio.h>
 #include<stdlib.h>
 using namespace std;
-
+float bal=2000.0;
+int pin=2468;
 class atm
 {
 private:
-    int choice,with_amt,dep_amt,pin_ver,votp,gpin1,gpinf,step,ced,back,camt;
+    int choice,pin_ver,votp,gpin1,gpinf,step,ced,back;
+    float camt,with_amt,dep_amt;
     
 public:
-    int choicef();
+   // int choicef();
     float with_money();
     float dep_money();
     int gpin();
     int verf(int pinv);
     int balc();
-    int pin=2468;
-    float bal;
+    
+    
 };
 
 atm p4;
@@ -34,42 +36,10 @@ int atm::verf(int pinv)
     }
 }
 
-//selecting choice (screen 1)
-int atm::choicef(/* args */)
-{  // atm p1;
-    system("CLS");
-   loc1: cout<<"     >>>select<<< "<<endl;
-    cout<<"1. Genrate Green PIN"<<endl;
-    cout<<"2. check balence"<<endl;
-    cout<<"3. withdraw money"<<endl;
-    cout<<"4. deposit money"<<endl;
-    cin>>choice;
-     switch (choice)
-    {
-    case 1:
-        p4.gpin();
-        break;
-    case 2:
-        p4.balc();
-        break;
-    case 3:
-        bal=p4.with_money();
-        break;
-    case 4:
-        bal=p4.dep_money();
-        break;    
-    default:
-        system("CLS");
-        cout<<"invalid input Try again";
-        goto loc1;
-        break;
 
-    }
-    return choice;
-}
 //green pin genration
 int atm::gpin() {
-    atm p2;
+    //atm p2;
     step=0;
    loc2:
    if(step==1)
@@ -105,11 +75,11 @@ cout<<"press 1 to go back"<<endl;
 cin>>back;
 if(back==1)
 {
-    p2.choicef();
+   // p2.choicef();
 }
 else
 {
-        p2.choicef();
+       // p2.choicef();
 }
 }
 
@@ -123,7 +93,7 @@ loc4:   cout<<"Enter you PIN"<<endl;
    {
    case 1:
    system("CLS");
-    cout<<"Your Current Balence: "<<camt<<endl;
+    cout<<"Your Current Balence: "<<bal<<endl;
     break;
    
    default:
@@ -136,11 +106,11 @@ cout<<"press 1 to go back"<<endl;
 cin>>back;
 if(back==1)
 {
-    p2.choicef();
+   // p2.choicef();
 }
 else
 {
-        p2.choicef();
+        //p2.choicef();
 }
 return 0;
 }
@@ -173,11 +143,11 @@ cout<<"press 1 to go back"<<endl;
 cin>>back;
 if(back==1)
 {
-    p2.choicef();
+   // p2.choicef();
 }
 else
 {
-        p2.choicef();
+       // p2.choicef();
 }
 return camt;
 }
@@ -210,11 +180,11 @@ cout<<"press 1 to go back"<<endl;
 cin>>back;
 if(back==1)
 {
-    p2.choicef();
+   // p2.choicef();
 }
 else
 {
-        p2.choicef();
+        //p2.choicef();
 }
 return camt;   
 }
@@ -226,29 +196,51 @@ return camt;
 
 int main()
 {   atm p1;
-    atm syss;
+   //atm syss;
+    int choice;
     system("CLS");
    // atm p1;
-   p1.choicef();
-    // switch (choice1)
-    // {
-    // case 1:
-    //     p1.gpin();
-    //     break;
-    // case 2:
-    //     p1.balc();
-    //     break;
-    // case 3:
-    //     p1.with_money();
-    //     break;
-    // case 4:
-    //     p1.dep_money();
-    //     break;    
-    // default:
-    //     cout<<"invalid input Try again";
-    //     goto loc1;
-    //     break;
+//    p1.choicef();
+   while (1)
+   {
+      loc1:  system("CLS");
+    cout<<"     >>>select<<< "<<endl;
+    cout<<"1. Genrate Green PIN"<<endl;
+    cout<<"2. check balence"<<endl;
+    cout<<"3. withdraw money"<<endl;
+    cout<<"4. deposit money"<<endl;
+    cin>>choice;
+   
+    switch (choice)
+    {
+    case 1:
+        p1.gpin();
+        break;
+    case 2:
+        p1.balc();
+        break;
+    case 3:
+        bal=p1.with_money();
+        break;
+    case 4:
+        bal=p1.dep_money();
+        break;    
+    default:
+        cout<<"invalid input Try again";
+        goto loc1;
+        break;
 
-    // }
+    } }
     return 0;
 }
+/*
+
+
+By: Karthik Prabhu
+github: https://github.com/karthikprabhu10
+Ver : 2.10
+update : 14 Oct 2023
+  -K4X
+
+
+*/
